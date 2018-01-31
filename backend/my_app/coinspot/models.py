534ee79852610582c04545bcc7e95b0f4e11984c
webdev522 <webdev522@gmail.com>
2018-01-31 21:39:29 +0100

    initial commit

---
+++ backend/my_app/coinspot/models.py
@@ -0,0 +1,5 @@
+MESSAGES = {
+	'default': 'Hello to the World of Flask!',
+	'key1': 'Text 1',
+	'key2': 'Text 2'
+}
---
+++ backend/my_app/coinspot/views.py
@@ -0,0 +1,16 @@
+from my_app import app
+from my_app.coinspot.models import MESSAGES
+
+@app.route('/')
+@app.route('/hello')
+def hello_world():
+    return MESSAGES['default']
+
+@app.route('/show/<key>')
+def get_message(key):
+    return MESSAGES.get(key) or "%s not found!" % key
+
+@app.route('/add/<key>/<message>')
+def add_or_update_message(key, message):
+    MESSAGE[key] = message
+    return "%s Added/Updated" % key
